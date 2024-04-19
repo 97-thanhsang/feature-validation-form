af1f68d12532afacbf1519f4c94bf9064d2ea4b9
Thanh Sang <anhsang31797@example.com>
2024-04-19 17:39:17 +0700

    init temp

---
+++ main.js
@@ -0,0 +1,13 @@
+function Validator(options){
+    var formElement = document.querySelector(options.form);
+    if (formElement) {
+        
+    }
+}
+
+Validator.isRequied = function () {
+    return 1;
+}
+Validator.isEmail = function () {
+    return 2;
+}
